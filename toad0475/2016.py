3aeca6ac4bb2a9b610c3c06d60deb909757eef52
toadwave <toad0475@gmail.com>
2019-10-14 20:16:21 +0900

    Update 2016.py

--- toad0475/2016.py
+++ toad0475/2016.py
@@ -3,3 +3,12 @@ def solution(a, b):
     numOfDysForEachMon = [31,29,31,30,31,30,31,31,30,31,30,31]
     return weekDays[(sum(numOfDysForEachMon[:a-1])+b)%7-1]
 
+'''
+def solution2(a, b):
+    weekDays = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    numOfDysForEachMon = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    numOfDay = 0
+    for i in range(a):
+        numOfDay += numOfDysForEachMon[i]
+    return weekDays[((numOfDay+b)%7)-1]
+'''
