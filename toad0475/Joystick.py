024d39fb36ae700de4fc238388610c5da2663c3f
toadwave <toad0475@gmail.com>
2019-10-18 15:16:43 +0900

    Update Joystick.py

--- toad0475/Joystick.py
+++ toad0475/Joystick.py
@@ -1,14 +1,34 @@
+# 현재 위치에서 가장 가까운 A가 아닌 문자 찾기           
+def greedy(count,name_len,notA_idxs):
+    curr_idx = 0
+    while notA_idxs:
+        dists = []
+        for idx in notA_idxs: # 요 부분이 아직 Brute force임!!
+            dists.append(min(abs(idx-curr_idx),name_len-abs(idx-curr_idx)))
+        min_dist = min(dists)
+        count+= min_dist
+        curr_idx = notA_idxs[dists.index(min_dist)]
+        notA_idxs.pop(dists.index(min_dist))
+    return count
+
 def solution(name):
-    alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    answer = 0
-    # 위아래 최소
-    for char in name:
-        if alphabet.index(char) > 0 and alphabet.index(char) <= 13:
-            answer += alphabet.index(char)
+    alpha='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    count = 0 # 아웃풋
+    alpha_len = len(alpha)
+    name_len = len(name)
+    notA_idxs = [] # name 중 A가 아닌 element의 index 리스트
+
+    for i in range(name_len):
+        if name[i] =='A':
+            pass
         else:
-            answer += alphabet.index(char) - ((alphabet.index(char)-13)*2)
-    # 양옆 최소?
-    for i in range(len(char)):
-        if
-        
-    return answer
+            count += min(alpha.index(name[i]), alpha_len - alpha.index(name[i]))
+            notA_idxs.append(i)    
+    if len(notA_idxs) == name_len: # A가 없다면
+        return count+(name_len-1)
+    else: # A가 하나라도 포함되어 있다면
+        return greedy(count, name_len, notA_idxs)        
+
+    
+# 테스트케이스
+print(solution("AAABAAAAAABA"))
