6559364702129508a75575cb9d5e5e5b1d2fb970
trippysour <55823151+trippysour@users.noreply.github.com>
2019-10-28 13:52:55 +0900

    Update 05_영어끝말잇기.py

--- trippysour/05_영어끝말잇기.py
+++ trippysour/05_영어끝말잇기.py
@@ -1,12 +1,14 @@
-words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "bot", "tank"]
-n = 3
 def solution(n, words):
     index = 0  # 현재 인덱스 단어
     next = index + 1  # 다음 인덱스 단어
+    wrong = 0
+    wrong1 = 0
     for i in words:
         list1 = list(words[index])  # 현재 인덱스 단어를 list1로
         list2 = list(words[next])  # 다음 인덱스 단어를 list2로
         if list1[-1] != list2[0]:  # 현재 리스트 마지막 인덱스 철자가 다음 리스트 첫 인덱스 철자와 다르면
+            wrong = wrong + 1 # 틀린게 있다고 표시
+            when1 = words.index(words[next]) + 1
             turn1 = ((words.index(words[next]) + 1) / n)  # 틀린 문자의 인덱스+1 값을 n으로 나눈 것이 횟수
             who1 = ((words.index(words[next]) + 1) % n)  # 틀린 문자의 인덱스+1값을 n으로 나눈 몫
             if who1 == 0: who1 = who1 + n  # 만약 몫이 0이라면 마지막 사람이니 n을 더해주고
@@ -15,35 +17,37 @@ def solution(n, words):
         index = index + 1
         next = next + 1
         if index == len(words) - 1:
-            break
-        else:  # 끝말잇기 잘 했다면 0,0
-            who1 = 0
-            turn1 = 0
+            break     
+    index = 0  # 현재 인덱스 단어
     if len(words) != len(set(words)):  # words리스트와 words의 set(중복된게 제거 된) len이 다르면
+        wrong1 = wrong1 + 1 # 틀린게 있다고 표시
         words.reverse() # words 역순으로 정렬
         for i in words:
             if words.count(words[index]) != 1: # 역순으로 된 리스트들의 요소들의 갯수가 1이 아니라면
-                number = len(words) - words.index(words[index]) # 그 인덱스를 words의 len에서 뺴서 원래 words에서 몇번쨰인지 찾기
-                turn2 = number / n  # 틀린 문자의 인덱스값을 n으로 나눈 것이 횟수
-                who2 = number % n  # 틀린 문자의 인덱스값을 n으로 나눈 몫
+                when2 = len(words) - words.index(words[index]) # 그 인덱스를 words의 len에서 뺴서 원래 words에서 몇번쨰인지 찾기
+                turn2 = when2 / n  # 틀린 문자의 인덱스값을 n으로 나눈 것이 횟수
+                who2 = when2 % n  # 틀린 문자의 인덱스값을 n으로 나눈 몫
                 if who2 == 0: who2 = who2 + n  # 만약 몫이 0이라면 마지막 사람이니 n을 더해주고
                 else: turn2 = int((turn2 + 1))  # 아니라면 turn을 다 못채웠으니 turn에 1을 해주자
                 break
             index = index + 1
-        else:
-            who2 = 0
-            turn2 = 0
-    if who1 < who2:
-        who = who1
-        turn = turn1
-    elif who1 > who2:
-        who = who2
-        turn = turn2
-    elif who1 == who2:
+            if index == len(words) - 1:
+                break
+    if wrong == 0 and wrong1 == 0: # 만약 틀린게 없다면
         who = 0
         turn = 0
+    elif wrong != 0 and wrong1 == 0: #만약 첫번째만 틀리면
+        who = who1
+        turn = turn1
+    elif wrong == 0 and wrong1 !=0:
+        who = who2
+        turn = turn2
+    elif wrong != 0 and wrong1 !=0:
+        if when1 < when2: # who1의 wrong이 먼저 발생했다면 who1이 who, turn1이 turn
+            who = who1
+            turn = turn1
+        else: # 아니라면 who2, turn2
+            who = who2
+            turn = turn2
     answer = [who, turn]
     return answer
-# 19번 문제 해결 해야함. 중복이 된 아이템 검사하기, 끝말잇기 잘 했나 둘다 서로 유기적으로 되야함..
-# 테스트 케이스 n=3 , ["tank", "kick", "know", "wheel", "land", "dream", "mother", "bot", "tank"] 일경우에 "bot"이 틀렸으므로 [2,3]이 나와야하는데 코드상에서 첫번째 tank 검사할 때 마지막 tank가 같다고 [3,3]을 반환
-# 조건문을 잘 써보자, 지금은 위 답은나오지만 조건에 의해서 다 변수가 정의되서 
