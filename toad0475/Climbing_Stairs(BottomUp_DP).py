c4ac95d02e00e5609d1d899f9a6c666e3741dd8f
toadwave <toad0475@gmail.com>
2019-11-27 23:47:34 +0900

    Update Climbing_Stairs(BottomUp_DP).py

--- toad0475/Climbing_Stairs(BottomUp_DP).py
+++ toad0475/Climbing_Stairs(BottomUp_DP).py
@@ -4,9 +4,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         f={}
         for i in range(1, n+1):
-            if i==1: f[i]=1
-            elif i==2: f[i]=2
+            if i<=2: f[i]=i
             else:
                 f[i]=f[i-1]+f[i-2]
         return f[n]
-        
