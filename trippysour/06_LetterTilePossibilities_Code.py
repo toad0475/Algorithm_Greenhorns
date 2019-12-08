13741bf1f2168df925f224ea0e3542768a71e7c7
trippysour <55823151+trippysour@users.noreply.github.com>
2019-12-08 23:47:29 +0900

    Delete 06_LetterTilePossibilities_Code.py

--- trippysour/06_LetterTilePossibilities_Code.py
+++
@@ -1,9 +0,0 @@
-tiles = 'AAC'
-r = len(tiles)
-letters = set() # 중복결과 제거를 위해 set 생성
-import itertools # 순열 기능
-for i in range(r):
-   letters.update(map(''.join, itertools.permutations(tiles, r))) # titles의 원소로 순열 만들기
-   r = r - 1   
-print(letters)
-print(len(letters))
