e546a0da9f44bb2e763090e97716a20d8f8cf50e
trippysour <55823151+trippysour@users.noreply.github.com>
2019-12-15 19:42:54 +0900

    Add files via upload

---
+++ trippysour/11_AddDigits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            dr_n = 0
+        elif num % 9 == 0:
+            dr_n = 9
+        else:
+            dr_n = 1 + ((num - 1) % 9)
+        return dr_n
+
+# 자릿수근 공식을 그대로 코드에 적용
+# https://leetcode.com/submissions/detail/286113898/
+# 28ms, 12.7MB
