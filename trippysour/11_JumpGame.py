00bb77a4e70dc80895c3b29006176a929bfcb7fc
trippysour <55823151+trippysour@users.noreply.github.com>
2020-01-06 23:35:34 +0900

    Update 11_JumpGame.py

--- trippysour/11_JumpGame.py
+++ trippysour/11_JumpGame.py
@@ -8,9 +8,8 @@ class Solution:
                 #print('maxjump :', maxjump, ',', 'i :',i , ',', 'v :', v, ',', 'i+v :',i+v)
                 if i <= maxjump: # 현재 i값이 maxjump보다 작거나 같으면 도달할 수 있는 곳이니까
                     maxjump = max(maxjump, i+v) # 그곳에서 현재 maxjump와 현재 i+v 값 충에 큰 값으로 maxjump 바꿈
-                if maxjump >= len(nums)-1: return True
-            else: return False
+             return maxjump >= len(nums) - 1
 
             
-# https://leetcode.com/submissions/detail/289664111/
-# Runtime: 96 ms, Memory Usage: 14.9 MB
+# https://leetcode.com/submissions/detail/291741798/
+# Runtime: 80 ms, Memory Usage: 14.9 MB
