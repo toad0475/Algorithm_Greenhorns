a7e87ffdae3dc4ed10a38bb605d7efcf075e69c3
toadwave <toad0475@gmail.com>
2020-01-06 09:40:34 +0900

    Update Jump_Game.py

--- toad0475/Jump_Game.py
+++ toad0475/Jump_Game.py
@@ -1,6 +1,9 @@
 # submission: https://leetcode.com/submissions/detail/289481517/
 
-# nums의 top-down으로 수행하며 nums의 끝에 도달할 수있는지 판단하는 알고리즘. nums 끝에 도달 가능하다고 판단되면 그 인덱스 값(lastpos)을 갱신한다. nums의 0번째 인덱스까지 연산을 마친 후 lastpos가이 0이면 True 그렇지 않으면 False를 반환.
+# nums를 top-down으로 수행하여 nums의 끝에 도달할 수 있는지 판단하는 알고리즘. 
+# nums 끝에 도달 가능하다고 판단되면 그 인덱스 값(lastpos)을 갱신한다. 
+# nums의 0번째 인덱스까지 연산을 마친 후 lastpos가이 0이면 True 그렇지 않으면 False를 반환.
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if len(nums)==1:
