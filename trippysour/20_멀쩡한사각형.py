b4c6819e833eb4587aafcbc42bbd14aaae688938
trippysour <55823151+trippysour@users.noreply.github.com>
2020-03-23 22:52:51 +0900

    Update 20_멀쩡한사각형.py

--- trippysour/20_멀쩡한사각형.py
+++ trippysour/20_멀쩡한사각형.py
@@ -1,17 +1,26 @@
+from math import gcd
+def solution(w, h):
+    greatestcommondivisior = gcd(w, h)
+    return (w*h)-((w+h)-greatestcommondivisior)
+
+#print(solution(3,3)) #6
+#print(solution(8,12)) #80
+#print(solution(3,5)) #8
+#print(solution(3,4)) #6
+#print(solution(5,4)) #6
+
+""" 아래 방법은 4번, 17번 오답, 11, 13,15,16,18번 런타임 오류 
 def solution(w,h):
     wh = w*h
-    dict = {}
-    hbyw = h/w
-    wbyh = w/h
-    for i in range(1, w):
-        dict[i] = hbyw*i
-    for i in range(1, h):
-        dict[wbyh*i] = i
+    if w == h: return wh * ((w-1)/w)
+    else:
+        dict = {}
+        hbyw = h/w
+        wbyh = w/h
+        for i in range(1, w):
+            dict[i] = hbyw*i
+        for i in range(1, h):
+            dict[wbyh*i] = i
         
-    return wh - (len(dict)+1)
-
-print(solution(3,3)) #6
-print(solution(8,12)) #80
-print(solution(3,5)) #8
-print(solution(3,4)) #6
-# 푸는중...
+        return wh - (len(dict)+1)
+"""
