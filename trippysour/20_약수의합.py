3f484c9a5eba940c12aaa8c1a667dce0a6284794
trippysour <55823151+trippysour@users.noreply.github.com>
2020-03-23 22:18:11 +0900

    Update 20_약수의합.py

--- trippysour/20_약수의합.py
+++ trippysour/20_약수의합.py
@@ -1,8 +1,8 @@
 def solution(n):
     if n == 0: return 0
     else:
-        answer = []
+        answer = 0
         for i in range(1, n):
             if n % i == 0: 
-                answer.append(n/i)
-        return sum(answer)+1
+                answer += n/i
+        return answer + 1
