9887fd9834fa905c7cd6ef5eb24f2f46e2679f1e
trippysour <55823151+trippysour@users.noreply.github.com>
2020-08-10 22:35:07 +0900

    Update 26_SortCharactersByFrequency.py

--- trippysour/26_SortCharactersByFrequency.py
+++ trippysour/26_SortCharactersByFrequency.py
@@ -14,3 +14,11 @@ class Solution:
                 result.append(lst[i][0])
 
         return ''.join(result)
+    
+# Runtime: 40~56 ms, faster than 85.17%, Memory: 15.5 MB
+# 아래는 Runtime: 20~32 ms, faster than 97.76%, Memory: 14.9MB
+# class Solution:
+#     def frequencySort(self, s: str) -> str:
+#         cnt = sorted([(k, v) for k, v in Counter(s).items()], key=lambda x:x[1], reverse=True)
+#         parts = [k * v for k, v in cnt]
+#         return ''.join(parts)
